74c1536c2c27e6ee81953864a8674b20d15c4a10
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-11-21 14:59:10 -0600

    readd filess

--- app.js
+++ app.js
@@ -4,14 +4,27 @@
 
 	function ArticleList(){
 		
-		this.data = [];
-		this.getArticleData(this.renderArticleList);
-
+		this.displayArticleList();
+	
 	}
 
 	ArticleList.prototype = {
+		/**
+		 * Bootstraps list.
+		 * 
+		 */
+		displayArticleList: function(){
+			this.data = [];
+			this.getArticleData(this.buildArticleList);
+		},
+		/**
+		 * Gets article JSON data and renders it via callback
+		 * 
+		 * @callback function to call after data has been successfully 
+		 * parsed
+		 *
+		 */		
 		getArticleData: function(callback){
-
 			var al = this;
 			var req = new XMLHttpRequest();
 			
@@ -19,20 +32,29 @@
 			
 			req.onload = function(){ 
 				
-				if (this.readyState != 4) return; // if not done yet, just get out of here
+				// if not done yet, just get out of here
+				if (this.readyState != 4) return; 
 
+				// if error status, generate error message
 				if (this.status != 200 && this.status != 304) { 
 					al.data = [{"Error": this.status + ' - ' + this.statusText}];
 				} else {
+					
+					// if 200 OK, parse and store data
 					al.data = JSON.parse(this.responseText);
 				}
 				
+				// render article list
 				callback.apply(al);
 			};
 			
 			req.send();
 		},
-		renderArticleList: function(){
+		/**
+		 * Populates article list
+		 * 
+		 */			
+		buildArticleList: function(){
 			
 			var output = '';
 			var al = this;
@@ -57,6 +79,38 @@
 			// Add articles to #article-container
 			document.querySelector('#article-container').innerHTML = output;
 		},
+		/**
+		 * Adds title case to article titles
+		 * (e.g. 'my article' -> 'My Article')
+		 * 
+		 */			
+		addTitleCase: function() {
+			this.data.forEach(function(a){
+				a.url_title = a.url_title.split(' ').map(function(word){
+					return word.charAt(0).toUpperCase() + word.substr(1);
+				}).join(' ');
+			});
+		},
+		/**
+		 * Compares article titles and reorders articles accordingly
+		 * 
+		 * @return int
+		 */
+		compareArticleTitles: function(a,b){
+
+			if (a.url_title < b.url_title) {
+				return -1;
+			} else if (a.url_title > b.url_title) {
+				return 1;
+			} else {
+				return 0;
+			}
+		},
+		/**
+		 * Adds HTML tags to article data
+		 * 
+		 * @return string
+		 */		
 		formatArticle: function(article){
 			var output = '<article>' +
 			'	<header>' +
@@ -73,29 +127,13 @@
 			'			URL Title: "' + article.url_title +'" '+ 
 			'		</p>' +
 			'	</header>' +
-			'</article>';
+			'</article><hr>';
 
 			return output;
-		},
-		compareArticleTitles: function(a,b){
-
-			if (a.url_title < b.url_title) {
-				return -1;
-			} else if (a.url_title > b.url_title) {
-				return 1;
-			} else {
-				return 0;
-			}
-		},
-		addTitleCase: function() {
-			this.data.forEach(function(a){
-				a.url_title = a.url_title.split(' ').map(function(word){
-					return word.charAt(0).toUpperCase() + word.substr(1);
-				}).join(' ');
-			});
 		}
 	};
 
-	app.articleList = new ArticleList(); // attaching ArticleList to the global app object allows to keep it separate from other units.
+	// Instantiate and save Article List instance into app
+	app.articleList = new ArticleList();
 
 })(window.app = window.app || {});
